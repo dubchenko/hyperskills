2cd84609c77a7c6c80b40881fa76dd4ddc99fb4a
Egor Dubchenko <dubchenkoegor@gmail.com>
2019-02-22 11:56:26 +0300

    commit

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    currentTab: 0,
+    userSkills: null,
   },
+  plugins: [createPersistedState()],
   mutations: {
-
+    setCurrentTab: (state, tabNumber) => state.currentTab = tabNumber,
+    setUserSkills: (state, userSkills) => state.userSkills = userSkills,
   },
-  actions: {
-
+  getters: {
+    currentTab: state => {
+      return state.currentTab
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <b-card no-body>
+    <b-tabs small card v-model="currentTab">
+      <b-tab title="General">
+        <div v-for="item in skills" class="">
+          {{ item.title }}
+          <input v-model="item.score">
+        </div>
+      </b-tab>
+      <button @click="save">Save</button>
+      <b-tab title="Edit profile">
+        <!-- <b-form-textarea
+          v-model="text"
+          placeholder="Enter something"
+          rows="3"
+          max-rows="6"
+        /> -->
+      </b-tab>
+      <b-tab title="Info">I'm the last tab</b-tab>
+    </b-tabs>
+  </b-card>
+  <div class="text-center">
+    <b-button-group class="mt-2">
+      <b-button @click="currentTab--">Previous</b-button>
+      <b-button @click="currentTab++">Next</b-button>
+    </b-button-group>
+    <div class="text-muted">Current Tab: {{ currentTab }}</div>
   </div>
+</div>
 </template>
 
 <script>
@@ -13,6 +38,40 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      skills: null
+    }
+  },
+  computed: {
+    currentTab: {
+      get: function() {
+				return this.$store.getters.currentTab;
+			},
+			set: function(value) {
+				this.$store.commit('setCurrentTab', value);
+			},
+    }
+  },
+  mounted() {
+    fetch('/data/skills.json')
+      .then(r => r.json())
+      .then(json => {
+        this.getSkills(json)
+      })
+  },
+  methods: {
+    getSkills(userSkills) {
+      if (this.$store.state.userSkills) {
+        this.skills = this.$store.state.userSkills
+      } else {
+        this.skills = userSkills
+      }
+    },
+    save() {
+      this.$store.commit('setUserSkills', this.skills)
+    }
+  },
 }
 </script>
